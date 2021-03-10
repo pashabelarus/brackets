0244e3187300f413ede1017c66bdc6377f35879d
Pavel Suntsou <pasha2010.94@gmail.com>
2021-03-10 23:25:32 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+const check = (str, bracketsConfig) => {
+  const stak = [];
+  const symbols = str.split('');
+  const isopen = (symbol) => bracketsConfig.some((pare) => pare[0] === symbol);
+  const isclose = (symbol) => bracketsConfig.some((pare) => pare[1] === symbol);
+  const isMatch = (s1 ,s2) => bracketsConfig.some(
+    (pare) => (pare[0] === s1 && pare[1] === s2) || (pare[0] === s2 && pare[1] === s1)
+  );
+
+  for (const symbol of symbols) {
+    const element = stak[stak.length - 1];
+
+    if (isopen(symbol) && isclose(symbol) && stak.length === 0 ) {
+      stak.push(symbol);
+      continue;
+    }
+
+    if (isopen(symbol) && isclose(symbol) && element === symbol) {
+      stak.pop();
+      continue;
+    }
+
+    if (isopen(symbol))  {
+      stak.push(symbol);
+      continue;
+    }
+
+    if (isclose(symbol) && isMatch(symbol , element))   {
+      stak.pop();
+      continue;
+    }
+
+    if (isclose(symbol) && !isMatch(symbol , element))   {
+      return false;
+    }
+  }
+  return stak.length === 0;
+};
+
+module.exports = check;
